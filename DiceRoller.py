2a565fad2cad1118fb9e012f09bc81e8d554797f
Scootz <emre.shively@gmail.com>
2022-04-05 20:44:00 -0400

    Simple DiceRoller + Tk demonstration

---
+++ DiceRoller.py
@@ -0,0 +1,18 @@
+import random
+
+class DiceRoller:
+    def __init__(self, num, sides):
+        self.num = num
+        self.sides = sides
+        self.result = []
+    
+    def roll(self):
+        self.result = random.choices(range(1, self.sides+1), k=self.num)
+
+    def displayResult(self, widget):
+        widget.config(text="You rolled {}d{} and got {} = {}".format(self.num, self.sides, self.result, sum(self.result)))
+        widget.grid(column=0, row=1)
+
+    def rollAndDisplay(self, widget):
+        self.roll()
+        self.displayResult(widget)
--- dndhelper.py
+++ dndhelper.py
@@ -1,10 +1,14 @@
 import tkinter as tk
 from tkinter import ttk
+import DiceRoller
 
 if __name__ == "__main__":
     root = tk.Tk()
     frame = ttk.Frame(root, padding=10)
     frame.grid()
-    ttk.Label(frame, text="Hello World!").grid(column=0, row=0)
-    ttk.Button(frame, text="Quit", command=root.destroy).grid(column=1, row=0)
+
+    diceRoller = DiceRoller.DiceRoller(2, 6)
+    l = ttk.Label(frame)
+    ttk.Button(frame, text="Roll 2d6", command=lambda: diceRoller.rollAndDisplay(l)).grid(column=0, row=0)
+
     root.mainloop()
