e18de96fe640a362f237cb8f6b8dd5207a691d2b
ed <ecadiz5@gmail.com>
2022-04-26 22:52:43 -0400

    Redo of gui

--- dndhelper.py
+++ dndhelper.py
@@ -34,24 +34,40 @@ class DNDHelper(ttk.Frame):
         self.createDiceRollerWidget()
 
     def createPlaylistWidget(self):
+
         self.playlistWidgetContainer = ttk.Frame(self, width=400, height=800)
         self.playlistWidgetContainer.grid(column=0, row=0, rowspan=3, padx=10, pady=10)
-        #self.playlistWidgetContainer.rowconfigure(0, weight=5)
-        #self.playlistWidgetContainer.rowconfigure(1, weight=1)
 
-        self.playlistContainer = ttk.Frame(self.playlistWidgetContainer, width=400, height=600, style="BW.TFrame")
-        self.playlistContainer.grid(column=0, row=0, padx=10, pady=10)
+        self.playlistControlsContainer = ttk.Frame(self.playlistWidgetContainer, width=410, height=100, style="BW.TFrame")
+        self.playlistControlsContainer.grid(column=0, row=0, rowspan=3, padx=10, pady=10)
+
+        self.playButton = ttk.Button(self.playlistControlsContainer, text="Play")
+        self.forwardButton = ttk.Button(self.playlistControlsContainer, text=">")
+        self.backButton = ttk.Button(self.playlistControlsContainer, text="<")
+        self.playButton.grid(column=1, row=1)        
+        self.forwardButton.grid(column=2, row=1)
+        self.backButton.grid(column=0, row=1)
+
+
+        self.tree = ttk.Treeview(self.playlistWidgetContainer, columns=("song_name", "artist_name"), show="headings", height=22)
+        self.tree.grid(column=0, row=3, padx=10, pady=10)   
+
+        self.tree.column("song_name")
+        self.tree.column("artist_name",width=100)
+        self.tree.heading("song_name",text="Name")
+        self.tree.heading("artist_name", text="Artist")
+        self.tree.tag_configure("simple", background="#E8E8E8")
 
         self.playlistButtonContainer = ttk.Frame(self.playlistWidgetContainer, width=400, height=120, style="BW.TFrame")
-        self.playlistButtonContainer.grid(column=0, row=1, padx=10, pady=10)
-        #self.playlistButtonContainer.columnconfigure(0, weight=1)
-        #self.playlistButtonContainer.columnconfigure(1, weight=1)
-        #self.playlistWidgetContainer.rowconfigure(0, weight=1)
+        self.playlistButtonContainer.grid(column=0, row=6, padx=10, pady=10)
 
+        self.createPlaylistButton = ttk.Button(self.playlistButtonContainer, text="Create Playlist")
         self.addSongButton = ttk.Button(self.playlistButtonContainer, text="Add Song")
-        self.swapPlaylistButton = ttk.Button(self.playlistButtonContainer, text="Swap Playlist")
-        self.addSongButton.grid(column=0, row=0, sticky='w')
-        self.swapPlaylistButton.grid(column=1, row=0, sticky='e')
+        self.deleteButton = ttk.Button(self.playlistButtonContainer, text="Delete")
+        self.createPlaylistButton.grid(column=0, row=0, sticky='w')
+        self.addSongButton.grid(column=1, row=0)
+        self.deleteButton.grid(column=2, row=0, sticky='e')
+
 
     def createMapWidget(self):
         self.mapWidgetContainer = ttk.Frame(self, width=800, height=800)
@@ -112,30 +128,7 @@ class DNDHelper(ttk.Frame):
         self.diceRollerLog.insert("1.0", "{}d{}: {}\n\n".format(self.diceRoller.num, self.diceRoller.sides, self.diceRoller.resultString()))
         self.diceRollerLog["state"] = "disabled"
 
-        
-
-
-def SongsTest():
-    s = Song("song1", "artist1", "url1",)
-    s1 = Song("song2", "artist1", "url2")
-    s2 = Song("song3", "artist1", "url3")
-    s3 = Song("ambience_song4", "artist1", "url4")
-    s4 = Song("epic_song", "artist1", "url5")
-
-    m = Playlist("music")
-    m.add(s)
-    m.add(s1)
-    m.add(s2)
-
-    a = Playlist("ambience")
-    a.add(s3)
-
-    mainPlaylist = Playlist("dndGame")
-    mainPlaylist.add(m)
-    mainPlaylist.add(a)
-    mainPlaylist.add(s4)
-    mainPlaylist.print()
-
+   
     
 if __name__ == "__main__":
     app = DNDHelper()
