18e67a43e64c3d88d1eb553cd207b5806fb185ba
Scootz <emre.shively@gmail.com>
2022-05-08 15:34:32 -0400

    restore

--- Map.py
+++ Map.py
@@ -1,2 +1,60 @@
-class Map:
-    
+import tkinter as tk
+from tkinter import ttk
+import os
+
+DEFAULT_BACKGROUND = "default.png"
+
+CIRCLE = 0
+RECTANGLE = 1
+
+class Map(tk.Canvas):
+    def __init__(self, parent, **kwargs):
+        # Create canvas and its scrollbars
+        self.scrollH = ttk.Scrollbar(parent, orient=tk.HORIZONTAL)
+        self.scrollV = ttk.Scrollbar(parent, orient=tk.VERTICAL)
+        super().__init__(parent, kwargs, xscrollcommand=self.scrollH.set, yscrollcommand=self.scrollV.set)
+        self.scrollH["command"] = self.xview
+        self.scrollV["command"] = self.yview
+        self.scrollH.grid(column=0, row=1, sticky=(tk.W, tk.E))
+        self.scrollV.grid(column=1, row=0, sticky=(tk.N, tk.S))
+
+        self.setBackground(DEFAULT_BACKGROUND)
+        self.tokens = []
+        self.AOE = []
+
+    def setBackground(self, path):
+        self.background = tk.PhotoImage(file=path)
+        self.config(scrollregion=(0, 0, self.background.width(), self.background.height()))
+        self.create_image(0, 0, image=self.background, anchor='nw')
+
+    def addToken(self, token):
+        if token not in self.tokens:
+            self.tokens.append(token)
+
+    def removeToken(self, token):
+        try:
+            self.tokens.remove(token)
+        except ValueError:
+            print("Token does not exist in the map.")
+
+    def addAOE(self, AOE):
+        if AOE not in self.AOE:
+            self.AOE.append(token)
+
+    def removeAOE(self, AOE):
+        try:
+            self.AOE.remove(AOE)
+        except ValueError:
+            print("AOE does not exist in the map.")
+
+class Token:
+    def __init__(self, size, height, position):
+        self.size = size
+        self.height = height
+        self.position = position
+
+class AOE:
+    def __init__(self, shape, size, center):
+        self.shape = shape
+        self.size = size
+        self.center = center
--- dndhelper.py
+++ dndhelper.py
@@ -2,6 +2,7 @@ import tkinter as tk
 from tkinter import ttk
 from tkinter import scrolledtext
 import DiceRoller
+import Map
 from Songs import Song, Playlist
 
 class DNDHelper(ttk.Frame):
@@ -11,7 +12,7 @@ class DNDHelper(ttk.Frame):
         self.createWidgets()
 
     def createWindow(self):
-        self.grid(sticky=tk.N+tk.S+tk.E+tk.W)
+        self.grid(sticky=(tk.N, tk.W, tk.E, tk.S))
         # Make the window resizeable
         root = self.winfo_toplevel()
         root.rowconfigure(0, weight=1)
@@ -57,18 +58,18 @@ class DNDHelper(ttk.Frame):
         self.mapWidgetContainer = ttk.Frame(self, width=800, height=800)
         self.mapWidgetContainer.grid(column=1, row=0, padx=10, pady=10)
 
-        self.mapContainer = ttk.Frame(self.mapWidgetContainer, width=800, height=600, style="BW.TFrame")
-        self.mapContainer.grid(column=0, row=0, padx=10, pady=10)
+        self.map = Map.Map(self.mapWidgetContainer, width=800, height=600)
+        self.map.grid(column=0, row=0, padx=10, pady=10, sticky=(tk.N, tk.W, tk.E, tk.S))
 
         self.mapButtonContainer = ttk.Frame(self.mapWidgetContainer, width=800, height=120, style="BW.TFrame")
-        self.mapButtonContainer.grid(column=0, row=1, padx=10, pady=10)
+        self.mapButtonContainer.grid(column=0, row=2, padx=10, pady=10, sticky=tk.S)
 
-        self.setBackgroundButton = ttk.Button(self.mapButtonContainer, text="Set Background")
+        self.setBackgroundButton = ttk.Button(self.mapButtonContainer, text="Set Background", command=self.setBackground)
         self.addCharacterButton = ttk.Button(self.mapButtonContainer, text="Add Character")
         self.addAOEButton = ttk.Button(self.mapButtonContainer, text="Add Area of Effect")
-        self.setBackgroundButton.grid(column=0, row=0, sticky='w')
+        self.setBackgroundButton.grid(column=0, row=0, sticky=tk.W)
         self.addCharacterButton.grid(column=1, row=0)
-        self.addAOEButton.grid(column=2, row=0, sticky='e')
+        self.addAOEButton.grid(column=2, row=0, sticky=tk.E)
 
     def createDiceRollerWidget(self):
         # Create containers
@@ -111,6 +112,10 @@ class DNDHelper(ttk.Frame):
         self.diceRollerLog["state"] = "normal"
         self.diceRollerLog.insert("1.0", "{}d{}: {}\n\n".format(self.diceRoller.num, self.diceRoller.sides, self.diceRoller.resultString()))
         self.diceRollerLog["state"] = "disabled"
+
+    def setBackground(self, *args):
+        bgpath = tk.filedialog.askopenfilename()
+        self.map.setBackground(self.bgpath)
 
         
 
