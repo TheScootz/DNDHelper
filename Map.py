48f34dab99fc5997fd4108d75e2bddefa350db62
Scootz <emre.shively@gmail.com>
2022-05-08 23:31:40 -0400

    Class support for adding/removing tokens and AOEs to/from the map. No GUI support yet. Unfortunately ovals do not support stipple on Windows

--- Map.py
+++ Map.py
@@ -6,6 +6,8 @@ from PIL import Image, ImageTk
 
 DEFAULT_BACKGROUND = "default.png"
 COLORS = ["red", "cyan", "blue", "purple", "magenta", "orange", "maroon", "green"]
+TOKEN_STIPPLE = "gray75"
+AOE_STIPPLE = "gray25"
 
 CIRCLE = 0
 RECTANGLE = 1
@@ -23,15 +25,19 @@ class Map(tk.Canvas):
 
         self.setBackground(DEFAULT_BACKGROUND)
         self.tokens = []
-        self.AOE = []
+        self.aoe = []
+        self.colors = COLORS
+        random.shuffle(self.colors)
 
-        random.shuffle(COLORS)
         self.testTokens()
 
     def testTokens(self):
-        self.addToken(Token(50, 10, (50,50)))
-        self.addToken(Token(30, 3, (67,324)))
+        self.addToken(Token(50, 0, (250,350)))
+        self.addToken(Token(30, 0, (67,324)))
         #self.removeToken(self.tokens[0])
+
+        self.addAOE(AOE(CIRCLE, 200, (300,300)))
+        self.addAOE(AOE(RECTANGLE, (100,200), (500,10)))
 
     def setBackground(self, path):
         self.background = ImageTk.PhotoImage(Image.open(path))
@@ -41,9 +47,16 @@ class Map(tk.Canvas):
 
     def addToken(self, token):
         if token in self.tokens: return
-
         self.tokens.append(token)
-        token.id = self.create_oval(*token.position, *[x+token.size for x in token.position], fill=COLORS.pop(0), width=5)
+        token.color = self.getColor()
+
+        pos_tl = [x-token.radius for x in token.position]
+        pos_br = [x+token.radius for x in token.position]
+        token.id = self.create_oval(*pos_tl, *pos_br, fill=token.color, width=5, stipple=TOKEN_STIPPLE, tag="token")
+        try:
+            self.tag_raise(token.id, "aoe")
+        # No AOEs exist
+        except tk.TclError: pass
 
     def removeToken(self, token):
         try:
@@ -51,32 +64,59 @@ class Map(tk.Canvas):
         except ValueError:
             print("Token does not exist in the map.")
             return
-
+            
+        self.colors.append(token.color)
         self.delete(token.id)
 
-    def addAOE(self, AOE):
-        if AOE not in self.AOE:
-            self.AOE.append(token)
-
-    def removeAOE(self, AOE):
+    def addAOE(self, aoe):
+        if aoe in self.aoe: return
+        self.aoe.append(aoe)
+        aoe.color = self.getColor()
+        
+        if aoe.shape == CIRCLE:
+            pos_tl = [x-aoe.size for x in aoe.position]
+            pos_br = [x+aoe.size for x in aoe.position]
+            aoe.id = self.create_oval(*pos_tl, *pos_br, fill=aoe.color, stipple=AOE_STIPPLE, tag="aoe")
+        elif aoe.shape == RECTANGLE:
+            pos_tl = aoe.position
+            pos_br = (aoe.position[0]+aoe.size[0], aoe.position[1]+aoe.size[1])
+            aoe.id = self.create_rectangle(*pos_tl, *pos_br, fill=aoe.color, stipple=AOE_STIPPLE, tag="aoe")
         try:
-            self.AOE.remove(AOE)
+            self.tag_lower(aoe.id, "token")
+        # No tokens exist
+        except tk.TclError: pass
+
+    def removeAOE(self, aoe):
+        try:
+            self.aoe.remove(aoe)
         except ValueError:
-            print("AOE does not exist in the map.")
+            print("aoe does not exist in the map.")
             return
 
+        colors.append(aoe.color)
+        self.delete(aoe.id)
+
+    def getColor(self):
+        try:
+            return self.colors.pop(0)
+        # If we're out of colors, refresh them
+        except IndexError:
+            self.colors = COLORS
+            random.shuffle(self.colors)
+            return self.colors.pop(0)
+
 class Token:
-    def __init__(self, size, height, position):
-        self.size = size
+    def __init__(self, radius, height, position, color="white", oid=0):
+        self.radius = radius
         self.height = height
         self.position = position
-        self.color = "white"
-        self.id = 0
+        self.color = color
+        self.id = oid
 
 class AOE:
-    def __init__(self, shape, size, center):
+    def __init__(self, shape, size, position, color="white", oid=0):
         self.shape = shape
         self.size = size
-        self.center = center
-        self.color = "white"
-        self.id = 0
+        self.position = position
+        self.color = color
+        self.id = oid
